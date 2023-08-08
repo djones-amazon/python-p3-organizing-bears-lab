c1d6c6f6d4e686ed026a3ac05d1e53cca90e58de
Devin Jones <devin.jones21083@gmail.com>
2023-08-07 22:58:53 -0400

    All tests passing.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 9, 'M', 'Brown', 'Docile', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, 'F', 'Burgundy', 'Aggressive', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 14, 'F', 'Black', 'Provocative', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'M', 'Auburn', 'Militant', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 7, 'F', 'Gold', 'Morose', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 11, 'M', 'Green', 'Grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, 'F', 'Obsidian', 'Bubbly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'M', 'Invisible', 'Mysterious', 1);
